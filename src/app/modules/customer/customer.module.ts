decc7092ab41e6f73e0f2e7516e4a4c7ddb1335e
Lesly Parra <parralesly2019@gmail.com>
2025-03-26 12:32:43 -0400

    contact component created

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { ListCustomersComponent } from './components/list-customers/list-customers.component';
 import { TableModule } from 'primeng/table';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    ListCustomersComponent
+    ListCustomersComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
