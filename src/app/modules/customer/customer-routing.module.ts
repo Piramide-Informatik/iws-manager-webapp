2465d6ecd1d48cb7dbceb6b73e70001fb241515f
Omar Selaya <201900837@est.umss.edu>
2025-03-28 16:05:43 -0400

    customer detail view

--- src/app/modules/customer/customer-routing.module.ts
+++ src/app/modules/customer/customer-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListCustomersComponent } from './components/list-customers/list-customers.component';
+import { DetailCustomerComponent } from './components/detail-customer/detail-customer.component';
 
 const routes: Routes = [
-  {path: 'list-customers', component: ListCustomersComponent}
+  {path: 'list-customers', component: ListCustomersComponent},
+  {path: 'detail-customer', component: DetailCustomerComponent}
 ];
 
 @NgModule({
--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -1,19 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { ListCustomersComponent } from './components/list-customers/list-customers.component';
 import { TableModule } from 'primeng/table';
+import { DetailCustomerComponent } from './components/detail-customer/detail-customer.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectModule } from 'primeng/select';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import { FloatLabel } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
 
 
 @NgModule({
   declarations: [
-    ListCustomersComponent
+    ListCustomersComponent,
+    DetailCustomerComponent
   ],
   imports: [
     CommonModule,
     CustomerRoutingModule,
-    TableModule
+    ReactiveFormsModule,
+    TableModule,
+    DropdownModule,
+    SelectModule,
+    InputTextModule,
+    TextareaModule,
+    FloatLabel,
+    ButtonModule,
+    Dialog
   ]
 })
 export class CustomerModule { }
