0e5915ab65a74f451c7b981527a66a4d24b939db
ArgenesDev <argenes77@gmail.com>
2025-03-28 16:15:03 -0400

    Resolved conflicts and merged changes

--- src/app/modules/work-contracts/work-contracts.module.ts
+++ src/app/modules/work-contracts/work-contracts.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { WorkContractsRoutingModule } from './work-contracts-routing.module';
 import { ListWorkContractsComponent } from './components/list-work-contracts/list-work-contracts.component';
 import { TableModule } from 'primeng/table';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { Dialog } from 'primeng/dialog';
 import { Ripple } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ConfirmDialog } from 'primeng/confirmdialog';
 import { InputTextModule } from 'primeng/inputtext';
@@ -19,6 +20,8 @@ import { SelectModule } from 'primeng/select';
 import { Tag } from 'primeng/tag';
 import { RadioButton } from 'primeng/radiobutton';
 import { Rating } from 'primeng/rating';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CalendarModule } from 'primeng/calendar';
 
 import { InputNumber } from 'primeng/inputnumber';
 import { IconFieldModule } from 'primeng/iconfield';
@@ -26,6 +29,7 @@ import { InputIconModule } from 'primeng/inputicon';
 import { Table } from 'primeng/table';
 import { DropdownModule } from 'primeng/dropdown';
 import { HttpClientModule } from '@angular/common/http';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [ListWorkContractsComponent],
@@ -34,6 +38,9 @@ import { HttpClientModule } from '@angular/common/http';
     WorkContractsRoutingModule,
     TableModule,
     ButtonModule,
+    InputNumberModule,
+    CalendarModule,
+    DialogModule,
     Dialog,
     Ripple,
     SelectModule,
@@ -50,10 +57,12 @@ import { HttpClientModule } from '@angular/common/http';
     Rating,
     InputTextModule,
     FormsModule,
+    ReactiveFormsModule,
     InputNumber,
     IconFieldModule,
     InputIconModule,
     HttpClientModule,
+    CheckboxModule,
   ],
 })
 export class WorkContractsModule {}
