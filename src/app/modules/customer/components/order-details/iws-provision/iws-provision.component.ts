b0b0544b646e1932db20ce277665baadf06dae7b
svaleria <svaleria.gonzales@gmail.com>
2025-04-07 22:25:46 -0400

    order details format and iws provision input done

--- src/app/modules/customer/components/order-details/iws-provision/iws-provision.component.ts
+++ src/app/modules/customer/components/order-details/iws-provision/iws-provision.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-iws-provision',
@@ -6,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './iws-provision.component.html',
   styleUrl: './iws-provision.component.scss'
 })
-export class IwsProvisionComponent {
+export class IwsProvisionComponent implements OnInit{
+  iwsEmployeeForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.iwsEmployeeForm = new FormGroup({
+      fixCommission: new FormControl('', [Validators.required]),
+      maxCommission: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.iwsEmployeeForm.valid) {
+      console.log(this.iwsEmployeeForm.value);
+    } else {
+      console.log("Formulario no válido");
+    }
+  }
 
 }
