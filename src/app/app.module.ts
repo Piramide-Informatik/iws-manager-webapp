f0ffaccba4535ac7c3a8e63a9974f8890ccb36ad
Omar Selaya <201900837@est.umss.edu>
2025-04-09 19:24:15 -0400

    sort app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
+//Angular 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from './core/layout/layout.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Lara from '@primeng/themes/lara';
-import Aura from '@primeng/themes/aura'//Tema default primeNG
-import { CustomerModule } from './modules/customer/customer.module';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
-import { EmployeeModule } from './modules/employee/employee.module';
-import { WorkContractsModule } from './modules/work-contracts/work-contracts.module';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { ButtonModule } from 'primeng/button';
+
+//Modules app
+import { AppRoutingModule } from './app-routing.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { LayoutModule } from './core/layout/layout.module';
 import { ProjectModule } from './modules/projects/project.module';
+import { WorkContractsModule } from './modules/work-contracts/work-contracts.module';
+
+//Prime NG
+import { ButtonModule } from 'primeng/button';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura'//Tema default primeNG
+import Lara from '@primeng/themes/lara';
+
+//Components
+import { AppComponent } from './app.component';
 import { OrdersModule } from './modules/orders/orders.module';
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -28,19 +34,19 @@ export function HttpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    OverlayPanelModule,
-    ButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
+    BrowserModule,
+    ButtonModule,
     CustomerModule,
     EmployeeModule,
-    WorkContractsModule,
-    OrdersModule,
-    ProjectModule,
-    MatButtonModule,
     HttpClientModule, // Agregado para que ngx-translate pueda cargar los archivos JSON
+    LayoutModule,
+    MatButtonModule,
+    OrdersModule,
+    OverlayPanelModule,
+    ProjectModule,
+    WorkContractsModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
