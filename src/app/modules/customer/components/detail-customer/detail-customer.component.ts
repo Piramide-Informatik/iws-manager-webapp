8b2a6b91097936ac71cebed0cec37ca1f093298a
Lesly Parra <parralesly2019@gmail.com>
2025-04-13 11:41:52 -0400

    errors fixed

--- src/app/modules/customer/components/detail-customer/detail-customer.component.ts
+++ src/app/modules/customer/components/detail-customer/detail-customer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Customer } from '../../../../Entities/customer';
