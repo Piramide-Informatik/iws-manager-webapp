12fe230524497d696494f2cbe96c25c4454a6de4
Omar Argenes Quispe <128398208+OmarArgenes@users.noreply.github.com>
2025-04-21 17:20:18 -0400

    final setting for master data submenu

--- src/app/core/sidebar-panels/masterdata-panel/masterdata-panel.component.ts
+++ src/app/core/sidebar-panels/masterdata-panel/masterdata-panel.component.ts
@@ -1,3 +1,4 @@
+import { ViewChild, ElementRef } from '@angular/core';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
@@ -17,6 +18,17 @@ export class MasterdataPanelComponent implements OnInit, OnDestroy {
   private langSubscription!: Subscription;
 
   constructor(private readonly translate: TranslateService) {}
+  @ViewChild('scrollMenu') scrollMenu!: ElementRef;
+
+  scroll(direction: 'left' | 'right') {
+    const container = this.scrollMenu.nativeElement as HTMLElement;
+    const scrollAmount = 200;
+
+    container.scrollBy({
+      left: direction === 'left' ? -scrollAmount : scrollAmount,
+      behavior: 'smooth',
+    });
+  }
 
   ngOnInit(): void {
     this.masterDataGroups = this.getMasterDataSidebar();
@@ -31,142 +43,111 @@ export class MasterdataPanelComponent implements OnInit, OnDestroy {
       this.langSubscription.unsubscribe();
     }
   }
-
   private getMasterDataSidebar(): any[] {
     return [
       {
-        label: this.translate.instant('SIDEBAR.GROUP_PERSONAS_Y_USUARIOS'),
-        items: [
-          {
-            label: this.translate.instant('SIDEBAR.USER'),
-            routerLink: ['/master-data/user'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.ROLES'),
-            routerLink: ['/master-data/roles'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.IWS_STAFF'),
-            routerLink: ['/master-data/iws-staff'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.IWS_COMMISSIONS'),
-            routerLink: ['/master-data/iws-commissions'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.IWS_TEAMS'),
-            routerLink: ['/master-data/iws-teams'],
-          },
-        ],
+        label: this.translate.instant('SIDEBAR.USER'),
+        routerLink: ['/master-data/user'],
       },
       {
-        label: this.translate.instant('SIDEBAR.GROUP_LOCALIZACION'),
-        items: [
-          {
-            label: this.translate.instant('SIDEBAR.COUNTRIES'),
-            routerLink: ['/master-data/countries'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.STATES'),
-            routerLink: ['/master-data/states'],
-          },
-        ],
+        label: this.translate.instant('SIDEBAR.ROLES'),
+        routerLink: ['/master-data/roles'],
       },
       {
-        label: this.translate.instant('SIDEBAR.GROUP_PARAMETROS_GENERALES'),
-        items: [
-          {
-            label: this.translate.instant('SIDEBAR.TITLE'),
-            routerLink: ['/master-data/title'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.ORDER_TYPES'),
-            routerLink: ['/master-data/order-types'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.APPROVAL_STATUS'),
-            routerLink: ['/master-data/approval-status'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.HOLIDAYS'),
-            routerLink: ['/master-data/holidays'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.ABSENCE_TYPES'),
-            routerLink: ['/master-data/absence-types'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.FUNDING_PROGRAMS'),
-            routerLink: ['/master-data/funding-programs'],
-          },
-        ],
+        label: this.translate.instant('SIDEBAR.IWS_STAFF'),
+        routerLink: ['/master-data/iws-staff'],
       },
       {
-        label: this.translate.instant('SIDEBAR.GROUP_PROYECTOS'),
-        items: [
-          {
-            label: this.translate.instant('SIDEBAR.PROJECT_STATUS'),
-            routerLink: ['/master-data/project-status'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.PROJECT_FUNNELS'),
-            routerLink: ['/master-data/project-funnels'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.REALIZATION_PROBABILITIES'),
-            routerLink: ['/master-data/realization-probabilities'],
-          },
-        ],
+        label: this.translate.instant('SIDEBAR.IWS_COMMISSIONS'),
+        routerLink: ['/master-data/iws-commissions'],
       },
       {
-        label: this.translate.instant('SIDEBAR.GROUP_FACTURACION'),
-        items: [
-          {
-            label: this.translate.instant('SIDEBAR.BILLERS'),
-            routerLink: ['/master-data/billers'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.DUNNING_LEVELS'),
-            routerLink: ['/master-data/dunning-levels'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.SALES_TAX'),
-            routerLink: ['/master-data/sales-tax'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.BILLING_METHODS'),
-            routerLink: ['/master-data/billing-methods'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.CONTRACT_STATUS'),
-            routerLink: ['/master-data/contract-status'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.TERMS_OF_PAYMENT'),
-            routerLink: ['/master-data/terms-of-payment'],
-          },
-        ],
+        label: this.translate.instant('SIDEBAR.IWS_TEAMS'),
+        routerLink: ['/master-data/iws-teams'],
       },
       {
-        label: this.translate.instant('SIDEBAR.GROUP_TECNICOS_Y_AYUDA'),
-        items: [
-          {
-            label: this.translate.instant('SIDEBAR.EMPLOYEE_QUALIFICATION'),
-            routerLink: ['/master-data/employee-qualification'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.NETWORKS'),
-            routerLink: ['/master-data/networks'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.SYSTEM_CONSTANTS'),
-            routerLink: ['/master-data/system-constants'],
-          },
-          {
-            label: this.translate.instant('SIDEBAR.TEXTS'),
-            routerLink: ['/master-data/texts'],
-          },
-        ],
+        label: this.translate.instant('SIDEBAR.COUNTRIES'),
+        routerLink: ['/master-data/countries'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.STATES'),
+        routerLink: ['/master-data/states'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.TITLE'),
+        routerLink: ['/master-data/title'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.ORDER_TYPES'),
+        routerLink: ['/master-data/order-types'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.APPROVAL_STATUS'),
+        routerLink: ['/master-data/approval-status'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.HOLIDAYS'),
+        routerLink: ['/master-data/holidays'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.ABSENCE_TYPES'),
+        routerLink: ['/master-data/absence-types'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.FUNDING_PROGRAMS'),
+        routerLink: ['/master-data/funding-programs'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.PROJECT_STATUS'),
+        routerLink: ['/master-data/project-status'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.PROJECT_FUNNELS'),
+        routerLink: ['/master-data/project-funnels'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.REALIZATION_PROBABILITIES'),
+        routerLink: ['/master-data/realization-probabilities'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.BILLERS'),
+        routerLink: ['/master-data/billers'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.DUNNING_LEVELS'),
+        routerLink: ['/master-data/dunning-levels'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.SALES_TAX'),
+        routerLink: ['/master-data/sales-tax'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.BILLING_METHODS'),
+        routerLink: ['/master-data/billing-methods'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.CONTRACT_STATUS'),
+        routerLink: ['/master-data/contract-status'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.TERMS_OF_PAYMENT'),
+        routerLink: ['/master-data/terms-of-payment'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.EMPLOYEE_QUALIFICATION'),
+        routerLink: ['/master-data/employee-qualification'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.NETWORKS'),
+        routerLink: ['/master-data/networks'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.SYSTEM_CONSTANTS'),
+        routerLink: ['/master-data/system-constants'],
+      },
+      {
+        label: this.translate.instant('SIDEBAR.TEXTS'),
+        routerLink: ['/master-data/texts'],
       },
     ];
   }
