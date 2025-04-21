fbb8ff53dc660c24048ee68857db7bfd168414df
svaleria <svaleria.gonzales@gmail.com>
2025-04-21 11:51:11 -0400

    routing and state page done

--- src/app/modules/master-data/master-data-routing.module.ts
+++ src/app/modules/master-data/master-data-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AbsenceTypesComponent } from './components/absence-types/absence-types.component';
+import { StatesComponent } from './components/states/states.component';
 
 const routes: Routes = [
   { path: '', component: AbsenceTypesComponent },
+  { path: 'states', component: StatesComponent },
 ];
 
 @NgModule({
