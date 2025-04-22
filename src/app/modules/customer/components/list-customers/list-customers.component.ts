e276ac68f486831d77d093673bb4963b6a0e7404
svaleria <svaleria.gonzales@gmail.com>
2025-04-22 11:21:38 -0400

    unused code removed

--- src/app/modules/customer/components/list-customers/list-customers.component.ts
+++ src/app/modules/customer/components/list-customers/list-customers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Customer } from '../../../../Entities/customer';
 import { CustomerService } from '../../services/customer.service';
 import { Table } from 'primeng/table';
@@ -77,12 +77,9 @@ export class ListCustomersComponent implements OnInit, OnDestroy {
   }
 
   reloadComponent(self: boolean, urlToNavigateTo?: string) {
-    //skipLocationChange:true means dont update the url to / when navigating
-    //console.log("Current route I am on:",this.router.url);
     const url = self ? this.router.url : urlToNavigateTo;
     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
       this.router.navigate([`/${url}`]).then(() => {
-        //console.log(`After navigation I am on:${this.router.url}`)
       })
     })
   }
