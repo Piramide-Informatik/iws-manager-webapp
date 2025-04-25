55517dce39bc237413451213f7369161e6791a3d
svaleria <svaleria.gonzales@gmail.com>
2025-04-25 11:22:33 -0400

    comments deleted

--- src/app/modules/projects/components/projects-overview/projects-overview.component.ts
+++ src/app/modules/projects/components/projects-overview/projects-overview.component.ts
@@ -98,12 +98,9 @@ export class ProjectsOverviewComponent implements OnInit, OnDestroy {
   }
 
   reloadComponent(self:boolean,urlToNavigateTo ?:string){
-    //skipLocationChange:true means dont update the url to / when navigating
-    //console.log("Current route I am on:",this.router.url);
    const url=self ? this.router.url :urlToNavigateTo;
    this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
      this.router.navigate([`/${url}`]).then(()=>{
-  //console.log(`After navigation I am on:${this.router.url}`)
      })
    })
  }
