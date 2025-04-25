2c476138be1938f6586a189e55e044ec9e3fceb9
Omar Argenes Quispe <128398208+OmarArgenes@users.noreply.github.com>
2025-04-25 05:28:52 -0400

    implemented funding programns UI and respective traslate

--- src/app/modules/master-data/master-data-routing.module.ts
+++ src/app/modules/master-data/master-data-routing.module.ts
@@ -7,6 +7,7 @@ import { ApprovalStatusProjectComponent } from './components/approval-status/com
 import { UserComponent } from './components/user/user.component';
 import { AddressComponent } from './components/address/address.component';
 import { HolidaysComponent } from './components/holidays/holidays.component';
+import { FundingProgramsComponent } from './components/funding-programs/funding-programs.component';
 
 const routes: Routes = [
   { path: 'absence-types', component: AbsenceTypesComponent },
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'approval-status', component: ApprovalStatusProjectComponent },
   { path: 'states', component: StatesComponent },
   { path: 'holidays', component: HolidaysComponent },
+  { path: 'funding-programs', component: FundingProgramsComponent },
 ];
 
 @NgModule({
