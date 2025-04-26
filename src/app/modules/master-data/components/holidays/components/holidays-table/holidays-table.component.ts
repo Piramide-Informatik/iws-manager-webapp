aa9f3dcd3484c9af33fea8e4ba31eb0825333763
Omar Argenes Quispe <128398208+OmarArgenes@users.noreply.github.com>
2025-04-26 00:31:33 -0400

    fixed resolved 4.0

--- src/app/modules/master-data/components/holidays/components/holidays-table/holidays-table.component.ts
+++ src/app/modules/master-data/components/holidays/components/holidays-table/holidays-table.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { Table } from 'primeng/table';
-import { Subscription } from 'rxjs';
-import { TranslateService, _ } from '@ngx-translate/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-holidays-table',
@@ -9,79 +6,53 @@ import { TranslateService, _ } from '@ngx-translate/core';
   templateUrl: './holidays-table.component.html',
   styleUrls: ['./holidays-table.component.scss'],
 })
-export class HolidaysTableComponent implements OnInit, OnDestroy {
-  holidays: any[] = [];
-  cols: any[] = [];
-  selectedColumns: any[] = [];
+export class HolidaysTableComponent {
+  holidays = [
+    { id: 1, sort: 1, name: 'Neujahr' },
+    { id: 2, sort: 2, name: 'Heilige Drei Könige' },
+    { id: 3, sort: 3, name: 'Rosenmontag' },
+    { id: 4, sort: 4, name: 'Internationaler Frauentag' },
+    { id: 5, sort: 5, name: 'Gründonnerstag' },
+    { id: 6, sort: 6, name: 'Karfreitag' },
+  ];
 
-  @ViewChild('dt2') dt2!: Table;
+  originalHolidays = [...this.holidays];
 
-  private langSubscription!: Subscription;
+  cols = [
+    { field: 'sort', header: 'Sort' },
+    { field: 'name', header: 'Feiertag' },
+  ];
 
-  constructor(private readonly translate: TranslateService) {}
+  selectedColumns = [...this.cols];
 
-  ngOnInit() {
-    this.holidays = [
-      { id: 1, sort: 1, name: 'Neujahr' },
-      { id: 2, sort: 2, name: 'Heilige Drei Könige' },
-      { id: 3, sort: 3, name: 'Rosenmontag' },
-      { id: 4, sort: 4, name: 'Internationaler Frauentag' },
-      { id: 5, sort: 5, name: 'Gründonnerstag' },
-      { id: 6, sort: 6, name: 'Karfreitag' },
-      { id: 7, sort: 7, name: 'Ostermontag' },
-      { id: 8, sort: 8, name: 'Tag der Arbeit' },
-      { id: 9, sort: 9, name: 'Pfingstmontag' },
-      { id: 10, sort: 10, name: 'Mariä Himmelfahrt' },
-      { id: 11, sort: 11, name: 'Allerheiligen' },
-      { id: 12, sort: 12, name: '1. Weihnachtstag' },
-    ];
+  applyFilter(event: Event, field: 'sort' | 'name') {
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
 
-    this.loadColHeaders();
-    this.selectedColumns = [...this.cols];
-
-    this.langSubscription = this.translate.onLangChange.subscribe(() => {
-      this.loadColHeaders();
-      this.selectedColumns = [...this.cols];
+    this.holidays = this.originalHolidays.filter((h) => {
+      const value = h[field];
+      return value.toString().toLowerCase().includes(filterValue);
     });
   }
 
-  loadColHeaders(): void {
-    this.cols = [
-      {
-        field: 'sort',
-        minWidth: 110,
-        header: this.translate.instant(_('HOLIDAYS.TABLE.SORT')),
-      },
-      {
-        field: 'name',
-        minWidth: 110,
-        header: this.translate.instant(_('HOLIDAYS.TABLE.NAME')),
-      },
-    ];
-  }
-
-  applyFilter(event: any, field: string) {
-    const inputElement = event.target as HTMLInputElement;
-    if (inputElement) {
-      this.dt2.filter(inputElement.value, field, 'contains');
-    }
-  }
-
   editHoliday(holiday: any) {
-    console.log('Editing', holiday);
+    console.log('Edit:', holiday);
   }
 
   deleteHoliday(id: number) {
-    console.log('Deleting ID', id);
+    this.holidays = this.holidays.filter((h) => h.id !== id);
+    this.originalHolidays = this.originalHolidays.filter((h) => h.id !== id);
   }
 
   createHoliday() {
-    console.log('Creating new holiday');
-  }
-
-  ngOnDestroy(): void {
-    if (this.langSubscription) {
-      this.langSubscription.unsubscribe();
-    }
+    const newHoliday = {
+      id: this.holidays.length + 1,
+      sort: this.holidays.length + 1,
+      name: 'Neuer Feiertag',
+    };
+    this.holidays.push(newHoliday);
+    this.originalHolidays.push(newHoliday);
+    console.log('Nuevo Feiertag', newHoliday);
   }
 }
