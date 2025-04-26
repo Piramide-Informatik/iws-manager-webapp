e11ddc12789a63f47aff20b0aeb234b0ca8217dc
Omar Argenes Quispe <128398208+OmarArgenes@users.noreply.github.com>
2025-04-26 01:44:14 -0400

    fixed resolved

--- src/app/modules/master-data/components/funding-programs/components/funding-programs-table/funding-programs-table.component.ts
+++ src/app/modules/master-data/components/funding-programs/components/funding-programs-table/funding-programs-table.component.ts
@@ -1,89 +1,57 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { Table } from 'primeng/table';
-import { Subscription } from 'rxjs';
-import { TranslateService, _ } from '@ngx-translate/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-funding-programs-table',
   standalone: false,
   templateUrl: './funding-programs-table.component.html',
-  styleUrl: './funding-programs-table.component.scss',
+  styleUrls: ['./funding-programs-table.component.scss'],
 })
-export class FundingProgramsTableComponent implements OnInit, OnDestroy {
-  fundingPrograms: any[] = [];
-  cols: any[] = [];
-  selectedColumns: any[] = [];
+export class FundingProgramsTableComponent {
+  fundingPrograms = [
+    { id: 1, program: 'BMWi', rate: 25 },
+    { id: 2, program: 'ZIM', rate: 45 },
+    { id: 3, program: 'Eurostars', rate: 30 },
+    { id: 4, program: 'Marketing', rate: 20 },
+    { id: 5, program: 'FUE-Verw', rate: 40 },
+    { id: 6, program: 'FZ', rate: 35 },
+    { id: 7, program: 'Go-Inno', rate: 28 },
+    { id: 8, program: 'GreenEconomy.IN.NRW', rate: 50 },
+    { id: 9, program: 'KMU-Innovativ', rate: 38 },
+    { id: 10, program: 'LuFo', rate: 32 },
+    { id: 11, program: 'Messe', rate: 22 },
+    { id: 12, program: 'NEXT.IN.NRW', rate: 27 },
+    { id: 13, program: 'Sonstiges', rate: 33 },
+    { id: 14, program: 'Studie', rate: 18 },
+  ];
 
-  @ViewChild('dt2') dt2!: Table;
+  originalFundingPrograms = [...this.fundingPrograms];
+  cols = [
+    { field: 'program', header: 'FUNDING.TABLE.PROGRAM' },
+    { field: 'rate', header: 'FUNDING.TABLE.RATE' },
+  ];
 
-  private langSubscription!: Subscription;
+  selectedColumns = [...this.cols];
 
-  constructor(
-    private readonly translate: TranslateService,
-    private readonly router: Router
-  ) {}
-
-  ngOnInit() {
-    this.fundingPrograms = [
-      { id: 1, name: 'EFRE', sort: 1 },
-      { id: 2, name: 'Eurostars', sort: 2 },
-      { id: 3, name: 'Horizon 2020', sort: 3 },
-    ];
-
-    this.loadColHeaders();
-    this.selectedColumns = [...this.cols];
-
-    this.langSubscription = this.translate.onLangChange.subscribe(() => {
-      this.loadColHeaders();
-      this.selectedColumns = [...this.cols];
+  applyFilter(event: Event, field: 'program' | 'rate') {
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+    this.fundingPrograms = this.originalFundingPrograms.filter((fp) => {
+      const value = fp[field].toString().toLowerCase();
+      return value.includes(filterValue);
     });
-  }
-
-  loadColHeaders(): void {
-    this.cols = [
-      {
-        field: 'name',
-        minWidth: 150,
-        header: this.translate.instant(_('FUNDING.TABLE.PROGRAM')),
-      },
-      {
-        field: 'sort',
-        minWidth: 60,
-        header: this.translate.instant(_('FUNDING.TABLE.RATE')),
-      },
-    ];
-  }
-
-  reloadComponent(self: boolean, urlToNavigateTo?: string) {
-    const url = self ? this.router.url : urlToNavigateTo;
-    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-      this.router.navigate([`/${url}`]).then(() => {});
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.langSubscription) {
-      this.langSubscription.unsubscribe();
-    }
   }
 
   editFundingProgram(program: any) {
-    console.log('Editing', program);
+    console.log('Editing program:', program);
   }
 
   deleteFundingProgram(id: number) {
-    console.log('Deleting ID', id);
+    this.fundingPrograms = this.fundingPrograms.filter((fp) => fp.id !== id);
+    this.originalFundingPrograms = [...this.fundingPrograms];
   }
 
   createFundingProgram() {
-    console.log('Creating new funding program');
-  }
-
-  applyFilter(event: any, field: string) {
-    const inputElement = event.target as HTMLInputElement;
-    if (inputElement) {
-      this.dt2.filter(inputElement.value, field, 'contains');
-    }
+    console.log('Creating new funding program...');
   }
 }
