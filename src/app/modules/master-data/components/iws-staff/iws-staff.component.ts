10e2eec9029b516c97c12fcf8fe56837d640a8b7
Omar Argenes Quispe <128398208+OmarArgenes@users.noreply.github.com>
2025-04-29 01:10:01 -0400

    feat(master-data): implement IWS Staff and Commissions management UI

---
+++ src/app/modules/master-data/components/iws-staff/iws-staff.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iws-staff',
+  standalone: false,
+  templateUrl: './iws-staff.component.html',
+  styleUrl: './iws-staff.component.scss'
+})
+export class IwsStaffComponent {
+
+}
