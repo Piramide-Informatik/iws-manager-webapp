837fef2fb5a4fc1e340dfbc31a52206a962882e9
svaleria <svaleria.gonzales@gmail.com>
2025-05-21 12:34:16 -0400

    modal created

---
+++ src/app/modules/master-data/components/title/components/title-modal/title-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-modal',
+  standalone: false,
+  templateUrl: './title-modal.component.html',
+  styleUrl: './title-modal.component.scss'
+})
+export class TitleModalComponent {
+
+}
