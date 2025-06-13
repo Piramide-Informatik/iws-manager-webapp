09333d9f08c2e3d444aab0bc796b8f693c4b4dd0
RodrigoGutierrez <rodrigo.gutierrez.acc@gmail.com>
2025-06-13 14:50:33 -0400

    Cleaned imports

--- src/app/modules/customer/components/detail-customer/detail-customer.component.ts
+++ src/app/modules/customer/components/detail-customer/detail-customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, inject, computed } from '@angular/core';
-import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Customer } from '../../../../Entities/customer';
 import { CustomerService } from '../../../../Services/customer.service';
