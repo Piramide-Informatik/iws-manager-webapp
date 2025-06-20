d6bf93844131ca611e3193a462cc5c34d355326c
svaleria <svaleria.gonzales@gmail.com>
2025-06-19 23:50:20 -0400

    states title implemented

--- src/app/modules/master-data/components/states/states.component.ts
+++ src/app/modules/master-data/components/states/states.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-states',
@@ -7,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './states.component.scss'
 })
 export class StatesComponent {
+  private langSubscription!: Subscription;
 
+  constructor(private readonly translate: TranslateService, private titleService: Title) { }
+  ngOnInit(): void {
+    this.updateTitle();
+    this.langSubscription = this.translate.onLangChange.subscribe(() => {
+      this.updateTitle();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.langSubscription) {
+      this.langSubscription.unsubscribe();
+    }
+  }
+
+  private updateTitle(): void {
+    this.titleService.setTitle(
+      this.translate.instant('PAGETITLE.STATES')
+    );
+  }
 }
