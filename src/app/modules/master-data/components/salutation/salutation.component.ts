0efb8a7e1f6e450e6d7bfc23ab0dc75fa9d94873
svaleria <svaleria.gonzales@gmail.com>
2025-06-19 23:48:05 -0400

    salutation title implemented

--- src/app/modules/master-data/components/salutation/salutation.component.ts
+++ src/app/modules/master-data/components/salutation/salutation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-salutation',
@@ -7,5 +10,25 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class SalutationComponent {
+  private langSubscription!: Subscription;
 
+  constructor(private readonly translate: TranslateService, private titleService: Title) { }
+  ngOnInit(): void {
+    this.updateTitle();
+    this.langSubscription = this.translate.onLangChange.subscribe(() => {
+      this.updateTitle();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.langSubscription) {
+      this.langSubscription.unsubscribe();
+    }
+  }
+
+  private updateTitle(): void {
+    this.titleService.setTitle(
+      this.translate.instant('PAGETITLE.SALUTATION')
+    );
+  }
 }
