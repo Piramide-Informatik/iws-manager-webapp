890f0210892831d2a8a463647b7be5575b2b9f91
Lesly Parra <parralesly17@gmail.com>
2025-07-08 02:12:14 -0400

    Added modal for create and edit employee contract

--- src/app/modules/work-contracts/components/ContractDetails/contract-details.component.ts
+++ src/app/modules/work-contracts/components/ContractDetails/contract-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { WorkContract } from '../../../../Entities/work-contracts';
 
 @Component({
   selector: 'app-contract-details',
@@ -8,16 +9,33 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './contract-details.component.html',
   styleUrl: './contract-details.component.scss'
 })
-export class ContractDetailsComponent implements OnInit {
+export class ContractDetailsComponent implements OnInit, OnChanges {
 
   ContractDetailsForm!: FormGroup;
-  public customer!: string;
+  @Input() modalType: string = "create";
+  @Input() customer!: string;
   employeeNumber: any;
+  @Input() workContract!: WorkContract;
+  @Output() isVisibleModal = new EventEmitter<boolean>();
 
   constructor(
     private readonly router: Router,
     private readonly activatedRoute: ActivatedRoute
   ) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    let workContractChange = changes['workContract'];
+    if (workContractChange && !workContractChange.firstChange) {
+      this.workContract = workContractChange.currentValue;
+      this.fillWorkContractForm();
+    }
+
+    let customerChange = changes['customer'];
+    if (customerChange && !customerChange.firstChange) {
+      this.customer = customerChange.currentValue;
+      this.ContractDetailsForm.get('customer')?.setValue(this.customer)
+    }
+  }
 
   ngOnInit(): void {
     this.ContractDetailsForm = new FormGroup({
@@ -34,15 +52,35 @@ export class ContractDetailsComponent implements OnInit {
       maxstudentag: new FormControl('', [Validators.required]),
       stundensatz: new FormControl('', [Validators.required]),
     });
-
-    this.activatedRoute.params
-      .subscribe(params => {
-        this.employeeNumber = params['employeeId'];
-        this.ContractDetailsForm.get('personalnr')?.setValue(this.employeeNumber);
-      })
   }
 
   goBackListContracts() {
     this.router.navigate(['../'], { relativeTo: this.activatedRoute });
+  }
+
+  get isCreateMode(): boolean {
+   return this.modalType !== 'delete';
+  }
+
+  closeModal(): void {
+    this.isVisibleModal.emit(false);
+    this.ContractDetailsForm.reset();
+  }
+
+  fillWorkContractForm() {
+    this.ContractDetailsForm.patchValue({
+      customer: this.customer,
+      personalnr: this.workContract.employeeId,
+      vorname: this.workContract.firstName,
+      nachname: this.workContract.lastName,
+      datum: this.workContract.startDate,
+      gehalt: this.workContract.salaryPerMonth,
+      wochenstunden: this.workContract.weeklyHours,
+      kurz: this.workContract.worksShortTime,
+      jahresauszahlung: this.workContract.specialPayment,
+      maxstudenmonat: this.workContract.maxHrspPerMonth,
+      maxstudentag: this.workContract.maxHrsPerDay,
+      stundensatz: this.workContract.hourlyRate
+    })
   }
 }
