e2603ea81ebe6832310266c4eaf46051cad7d506
FabDevX <201809660@est.umss.edu>
2025-07-08 15:17:49 +0100

    feat: service and utils is implemented to employment-contracts

---
+++ src/app/Entities/employment-contract.ts
@@ -0,0 +1,25 @@
+import { Customer } from './customer';
+import { Employee } from './employee';
+
+export interface EmploymentContract {
+  id?: number;
+  version?: number;
+  createdAt?: string;   
+  updatedAt?: string;
+  
+  hourlyRate: number;
+  hourlyRealRate: number;
+  hoursPerWeek: number;
+  maxHoursPerDay: number;
+  maxHoursPerMonth: number;
+  salaryPerMonth: number;
+  specialPayment: number;
+  startDate: string;   
+  workShortTime: number;
+  
+  customer: Customer | null;
+  employee: Employee | null;
+}
+
+
+
