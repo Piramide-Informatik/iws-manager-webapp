918a67e63cd265a49cb28826341af5aa3239e82f
Lesly Parra <parralesly17@gmail.com>
2025-07-14 01:41:50 -0400

    Fixed sonar issues

--- src/app/modules/contractor/components/contractor-details/contractor-details.component.ts
+++ src/app/modules/contractor/components/contractor-details/contractor-details.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, inject, Input, Output, EventEmitter } from '@angular/core';
+import { Component, inject, Input, Output, EventEmitter } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { CountryUtils } from '../../../master-data/components/countries/utils/country-util';
 @Component({
@@ -10,7 +9,7 @@ import { CountryUtils } from '../../../master-data/components/countries/utils/co
   templateUrl: './contractor-details.component.html',
   styleUrl: './contractor-details.component.scss'
 })
-export class ContractorDetailsComponent implements OnInit {
+export class ContractorDetailsComponent {
 
   private readonly countryUtils = inject(CountryUtils);
   countries = toSignal(
@@ -28,9 +27,7 @@ export class ContractorDetailsComponent implements OnInit {
   @Output() isContractVisibleModal = new EventEmitter<boolean>();
 
   constructor(
-    private readonly fb: FormBuilder,
-    private readonly router: Router, 
-    private readonly activatedRoute: ActivatedRoute
+    private readonly fb: FormBuilder
   ) {
     this.contractorForm = this.fb.group({
       customer: [''],
@@ -46,10 +43,6 @@ export class ContractorDetailsComponent implements OnInit {
 
   get isCreateContractorMode(): boolean {
     return this.modalContractType !== 'delete';
-  }
-
-  ngOnInit():void {
-    this.activatedRoute.params
   }
 
   onSubmit() {
