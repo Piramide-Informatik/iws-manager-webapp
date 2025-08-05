079aea8d3e445f2787d03363a0852c5b8e7076ef
Lesly Parra <parralesly17@gmail.com>
2025-08-05 00:48:35 -0400

    Issues fixed

--- src/app/modules/work-contracts/components/ContractDetails/contract-details.component.ts
+++ src/app/modules/work-contracts/components/ContractDetails/contract-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnDestroy, Output } from '@angular/core';
 import { FormGroup} from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { TranslateService } from '@ngx-translate/core';
