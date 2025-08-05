a23e981bf1966b01c6ed96d63f95da2608444de4
BeitaGonzales <vbeatriz.gonzales@gmail.com>
2025-08-05 11:07:52 -0400

    entities fixed

--- src/app/Entities/project.ts
+++ src/app/Entities/project.ts
@@ -5,15 +5,45 @@ export interface Project {
   createdAt: string;   // ISO format: "2025-06-17T06:21:35.281056"
   updatedAt: string;
   version: number;
-  projectLabel: string;
-  projectName: string;
-  fundingProgram: string;
-  promoter: string;
-  fundingLabel: string;
-  startDate: string;
-  endDate: string;
-  authDate: string;
-  fundingRate: string;
-  customer: Customer
+
+  customer: Customer | null;
+  
+  approvalDate?: String;
+  authorizationDate?: String;
+  chance?: Number;
+  comment?: String;
+  date1?: String;
+  date2?: String;
+  date3?: String;
+  date4?: String;
+  date5?: String;
+  endApproval?: String;
+  endDate?: String;
+  financeAuthority?: String;
+  fundingLabel?: String;
+  fundingRate?: Number;
+  hourlyRateMueu?: Number;
+  income1?: Number;
+  income2?: Number;
+  income3?: Number;
+  income4?: Number;
+  income5?: Number;
+  maxHoursPerMonth?: Number;
+  maxHoursPerYear?: Number;
+  orderIdFue?: Number;
+  orderIdAdmin?: Number;
+  stuffFlat?: Number;
+  productiveHoursPerYear?: Number;
+  projectLabel?: String;
+  projectName?: string;
+  note?: String;
+  shareResearch?: Number;
+  startApproval?: String;
+  startDate?: String;
+  title?: String;
+
+
+  // fundingProgram: FundingProgram | null;
+  // promoter: Promoter | null;  
 }
   
