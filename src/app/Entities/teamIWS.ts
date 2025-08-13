1f24733cdc47c5af0a520f6124b461112ac3c101
Omar Selaya <201900837@est.umss.edu>
2025-08-13 11:59:03 -0400

    fix sonar

--- src/app/Entities/teamIWS.ts
+++ src/app/Entities/teamIWS.ts
@@ -1,12 +1,10 @@
-import { EmployeeIws } from "./employeeIws";
-
 export interface TeamIws {
   id: number;
   createdAt: string;   // ISO format: "2025-06-17T06:21:35.281056"
   updatedAt: string;
   version: number;
 
-  teamleader: EmployeeIws | null; // reference employeeiws
+  teamleader: number; // consult
 
   teamiws?: string;
 }
