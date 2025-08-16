1eb4250994b64b6b473ec961b566b53548e83f16
Lesly Parra <parralesly17@gmail.com>
2025-08-15 22:58:36 -0400

    populate funding data

--- src/app/Entities/fundingProgram.ts
+++ src/app/Entities/fundingProgram.ts
@@ -1,12 +1,12 @@
 export interface FundingProgram {
   id: number;
-  createdAt: string;   // ISO format: "2025-06-17T06:21:35.281056"
-  updatedAt: string;
-  version: number;
+  createdAt?: string;   // ISO format: "2025-06-17T06:21:35.281056"
+  updatedAt?: string;
+  version?: number;
 
-  defaultfundingrate?: number;
-  defaulthoursperyear?: number;
-  defaultresearchshare?: number;
-  defaultstuffflat?: number;
-  fundingprogram?: string;
+  defaultFundingRate?: number;
+  defaultHoursPerYear?: number;
+  defaultResearchShare?: number;
+  defaultStuffFlat?: number;
+  name?: string;
 }
