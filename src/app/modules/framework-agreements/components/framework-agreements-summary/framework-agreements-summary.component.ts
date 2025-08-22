eac9c4f04a2b025e0a1bc9573954620f27928712
Lesly Parra <parralesly17@gmail.com>
2025-08-21 23:07:22 -0400

    Fix delete message in famework agreement delete modal

--- src/app/modules/framework-agreements/components/framework-agreements-summary/framework-agreements-summary.component.ts
+++ src/app/modules/framework-agreements/components/framework-agreements-summary/framework-agreements-summary.component.ts
@@ -59,6 +59,7 @@ export class FrameworkAgreementsSummaryComponent implements OnInit, OnDestroy {
         this.frameworkAgreements = fas.reduce((acc: any[], curr) => {
            acc.push({
              id: curr.id,
+             contractLabel: curr.contractLabel,
              contractno: curr.contractNo,
              frameworkContract: curr.contractLabel,
              date: curr.date,
@@ -127,14 +128,19 @@ export class FrameworkAgreementsSummaryComponent implements OnInit, OnDestroy {
         },
         error: () => {
           this.commonMessageService.showErrorDeleteMessage();
+          this.handleFinishRequest();
         },
         complete: () => {
-          this.selectedFrameworkAgreement = undefined;
-          this.visibleFrameworkAgreementModal = false;
-          this.isFrameworkAgreementLoading = false;
+          this.handleFinishRequest();
         }
       })
     }
+  }
+
+  handleFinishRequest() {
+    this.selectedFrameworkAgreement = undefined;
+    this.visibleFrameworkAgreementModal = false;
+    this.isFrameworkAgreementLoading = false;
   }
 
   createFrameworkAgreementDetail() {
