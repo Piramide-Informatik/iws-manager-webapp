1973d844251a1979333ad88b4154c1cd051660fd
The-Rigo <rodri.ledezma9483@gmail.com>
2025-09-01 17:02:33 -0400

    Change employeedIws edit

--- src/app/modules/master-data/components/iws-staff/components/edit-iws-staff/edit-iws-staff.component.ts
+++ src/app/modules/master-data/components/iws-staff/components/edit-iws-staff/edit-iws-staff.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { EmployeeIws } from '../../../../../../Entities/employeeIws';
+import {
+  BehaviorSubject,
+  Subscription
+} from 'rxjs';
+import { EmployeeIwsStateService } from '../../utils/employee-iws-state.service';
+import { EmployeeIwsUtils } from '../../utils/employee-iws-utils';
+import { TranslateService } from '@ngx-translate/core';
+import { MessageService } from 'primeng/api';
+import {
+  momentCreateDate,
+  momentFormatDate,
+} from '../../../../../shared/utils/moment-date-utils';
+import moment from 'moment';
 @Component({
   selector: 'app-edit-iws-staff',
   standalone: false,
@@ -8,7 +21,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrl: './edit-iws-staff.component.scss',
 })
 export class EditIwsStaffComponent implements OnInit {
+  public showOCCErrorModaEmployeeIws = false;
+  currentEmployeeIws: EmployeeIws | null = null;
   editIwsStaffForm!: FormGroup;
+
+  isSaving = false;
+  private readonly subscriptions = new Subscription();
+  private readonly editEmployeeIwsSource =
+    new BehaviorSubject<EmployeeIws | null>(null);
 
   teams = [
     { name: 'Team 1', code: 'T1' },
@@ -16,7 +36,24 @@ export class EditIwsStaffComponent implements OnInit {
     { name: 'Team 3', code: 'T3' },
   ];
 
+  constructor(
+    private readonly employeeIwsUtils: EmployeeIwsUtils,
+    private readonly employeeIwsStateService: EmployeeIwsStateService,
+    private readonly messageService: MessageService,
+    private readonly translate: TranslateService
+  ) {}
+
   ngOnInit(): void {
+    this.initForm();
+    this.setupEmployeeIwsSubscription();
+    const savedPublicHolidayId = localStorage.getItem('selectedEmployeeIwsId');
+    if (savedPublicHolidayId) {
+      this.loadEmployeeIwsAfterRefresh(savedPublicHolidayId);
+      localStorage.removeItem('selectedEmployeeIwsId');
+    }
+  }
+
+  private initForm(): void {
     this.editIwsStaffForm = new FormGroup({
       staffId: new FormControl('', [Validators.required]),
       shortName: new FormControl('', [Validators.required]),
@@ -29,7 +66,90 @@ export class EditIwsStaffComponent implements OnInit {
     });
   }
 
+  private setupEmployeeIwsSubscription(): void {
+    this.subscriptions.add(
+      this.employeeIwsStateService.currentEmployeeIws$.subscribe(
+        (EmployeeIws) => {
+          this.currentEmployeeIws = EmployeeIws;
+          EmployeeIws
+            ? this.loadEmployeeIwsData(EmployeeIws)
+            : this.clearForm();
+        }
+      )
+    );
+  }
+
+  private loadEmployeeIwsData(employeeIws: EmployeeIws): void {
+    this.editIwsStaffForm.patchValue({
+      staffId: employeeIws.employeeNo,
+      shortName: employeeIws.employeeLabel,
+      firstName: employeeIws.firstname,
+      lastName: employeeIws.lastname,
+      email: employeeIws.mail,
+      team: employeeIws.teamIws,
+      staffSince: employeeIws.startDate
+        ? moment(employeeIws.startDate, 'YYYY-MM-DD').toDate()
+        : null,
+      staffUntil: employeeIws.endDate
+        ? moment(employeeIws.endDate, 'YYYY-MM-DD').toDate()
+        : null,
+    });
+  }
+
+  private loadEmployeeIwsAfterRefresh(employeeIwsId: string): void {
+    this.isSaving = true;
+    this.subscriptions.add(
+      this.employeeIwsUtils
+        .getEmployeeIwsById(Number(employeeIwsId))
+        .subscribe({
+          next: (employeeIws) => {
+            if (employeeIws) {
+              this.employeeIwsStateService.setEmployeeIwsToEdit(employeeIws);
+            }
+            this.isSaving = false;
+          },
+          error: () => {
+            this.isSaving = false;
+          },
+        })
+    );
+  }
+
   onSubmit(): void {
+    if (
+      this.editIwsStaffForm.invalid ||
+      !this.currentEmployeeIws ||
+      this.isSaving
+    ) {
+      this.markAllAsTouched();
+      return;
+    }
+
+    this.isSaving = true;
+    const updateEmployeeIws: EmployeeIws = {
+      ...this.currentEmployeeIws,
+      employeeNo: this.editIwsStaffForm.value.staffId,
+      employeeLabel: this.editIwsStaffForm.value.shortName,
+      firstname: this.editIwsStaffForm.value.firstName,
+      lastname: this.editIwsStaffForm.value.lastName,
+      mail: this.editIwsStaffForm.value.email,
+      startDate: momentFormatDate(
+        momentCreateDate(this.editIwsStaffForm.value.staffSince)
+      ),
+      endDate: this.editIwsStaffForm.value.staffUntil
+        ? momentFormatDate(
+            momentCreateDate(this.editIwsStaffForm.value.staffUntil)
+          )
+        : '',
+      teamIws: null,
+    };
+
+    this.subscriptions.add(
+      this.employeeIwsUtils.updateEmployeeIws(updateEmployeeIws).subscribe({
+        next: (savedEmployeeIws) => this.handleSaveSuccess(savedEmployeeIws),
+        error: (err) => this.handleError(err),
+      })
+    );
     if (this.editIwsStaffForm.valid) {
       console.log(this.editIwsStaffForm.value);
     } else {
@@ -37,8 +157,61 @@ export class EditIwsStaffComponent implements OnInit {
     }
   }
 
+  private markAllAsTouched(): void {
+    Object.values(this.editIwsStaffForm.controls).forEach((control) => {
+      control.markAsTouched();
+      control.markAsDirty();
+    });
+  }
+
+  private handleSaveSuccess(savedEmployeeIws: EmployeeIws): void {
+    this.messageService.add({
+      severity: 'success',
+      summary: this.translate.instant('MESSAGE.SUCCESS'),
+      detail: this.translate.instant('MESSAGE.UPDATE_SUCCESS'),
+    });
+    this.employeeIwsStateService.setEmployeeIwsToEdit(null);
+    this.clearForm();
+  }
+
+  private handleError(err: any): void {
+    if (
+      err.message ===
+      'Version conflict: EmployeeIws has been updated by another user'
+    ) {
+      this.showOCCErrorModaEmployeeIws = true;
+    } else {
+      this.handleSaveError(err);
+    }
+    this.isSaving = false;
+  }
+
+  private handleSaveError(error: any): void {
+    console.error('Error saving employeeIWS:', error);
+    this.messageService.add({
+      severity: 'error',
+      summary: this.translate.instant('MESSAGE.ERROR'),
+      detail: this.translate.instant('MESSAGE.UPDATE_FAILED'),
+    });
+    this.isSaving = false;
+  }
+
   cancelEdit(): void {
     console.log('Edición cancelada');
     this.editIwsStaffForm.reset();
+  }
+  clearForm(): void {
+    this.editIwsStaffForm.reset();
+    this.currentEmployeeIws = null;
+    this.isSaving = false;
+  }
+  onRefresh(): void {
+    if (this.currentEmployeeIws?.id) {
+      localStorage.setItem(
+        'selectedPublicHolidayId',
+        this.currentEmployeeIws.id.toString()
+      );
+      window.location.reload();
+    }
   }
 }
