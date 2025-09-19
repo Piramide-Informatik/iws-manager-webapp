a3595595c12f029de5f42e38048bd672dff2efdc
The-Rigo <rodri.ledezma9483@gmail.com>
2025-09-19 00:24:53 -0400

    Implement RoleRight creation in function table

--- src/app/Entities/rightRole.ts
+++ src/app/Entities/rightRole.ts
@@ -2,13 +2,13 @@ import { SystemFunction } from "./systemFunction";
 import { Role } from "./role";
 
 export interface RightRole {
-    id: number;
-    createdAt: string; // ISO: "2025-06-17T06:21:35.281056"
-    updatedAt: string;
-    version: number;
+    id?: number;
+    createdAt?: string; // ISO: "2025-06-17T06:21:35.281056"
+    updatedAt?: string;
+    version?: number;
 
     accessRight?: number;
     sequenceNo?: number;
-    role?: Role | null;
-    systemFunction?: SystemFunction | null;
+    role: { id: number; name?: string };
+    systemFunction: { id: number; functionName?: string };
 }
