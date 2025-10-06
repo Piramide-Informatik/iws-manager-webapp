d944ac940058db034dbafe733d1e7592b7b79d67
svaleria <svaleria.gonzales@gmail.com>
2025-10-06 17:24:25 -0400

    feat: Add title to Roles Module

--- src/app/modules/master-data/components/roles/roles.component.ts
+++ src/app/modules/master-data/components/roles/roles.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageTitleService } from '../../../../shared/services/page-title.service';
 
 @Component({
   selector: 'app-roles',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.scss'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit{
+  constructor(private readonly pageTitleService: PageTitleService) {}
+
+  ngOnInit(): void {
+    this.pageTitleService.setTranslatedTitle('PAGETITLE.MASTER_DATA.ROLE');
+  }
 
 }
