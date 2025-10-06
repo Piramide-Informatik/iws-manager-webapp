6f4d5e3ceda0109bbfe1a1c8b1c01527d670a5c1
svaleria <svaleria.gonzales@gmail.com>
2025-10-06 19:44:12 -0400

    added title to text module

--- src/app/modules/master-data/components/text/text.component.ts
+++ src/app/modules/master-data/components/text/text.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageTitleService } from '../../../../shared/services/page-title.service';
 
 @Component({
   selector: 'app-text',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './text.component.html',
   styleUrl: './text.component.scss'
 })
-export class TextComponent {
-
+export class TextComponent implements OnInit {
+  constructor(private readonly pageTitleService: PageTitleService) { }
+  ngOnInit(): void {
+    this.pageTitleService.setTranslatedTitle('PAGETITLE.MASTER_DATA.TEXTS');
+  }
 }
