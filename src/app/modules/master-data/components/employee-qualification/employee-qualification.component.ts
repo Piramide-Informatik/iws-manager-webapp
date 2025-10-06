a1f7ff55203b9fc4ed631e3d69f635af4c2a336c
svaleria <svaleria.gonzales@gmail.com>
2025-10-06 17:45:10 -0400

    added title to employee qualification module

--- src/app/modules/master-data/components/employee-qualification/employee-qualification.component.ts
+++ src/app/modules/master-data/components/employee-qualification/employee-qualification.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { PageTitleService } from "../../../../shared/services/page-title.service";
 @Component({
   selector: 'app-employee-qualification',
   standalone: false,
   templateUrl: './employee-qualification.component.html',
   styleUrl: './employee-quelification.component.scss'
 })
-export class EmployeeQualificationComponent {
+export class EmployeeQualificationComponent implements OnInit {
+
+  constructor(private readonly pageTitleService: PageTitleService) { }
+
+  ngOnInit(): void {
+    this.pageTitleService.setTranslatedTitle('PAGETITLE.MASTER_DATA.EMPLOYEE_QUALIFICATION');
+  }
 
 }
