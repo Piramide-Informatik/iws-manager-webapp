3a112ce4a53892f008ae009008fda177f76eb190
svaleria <svaleria.gonzales@gmail.com>
2025-10-06 19:28:45 -0400

    added title to project status

--- src/app/modules/master-data/components/project-status/project-status.component.ts
+++ src/app/modules/master-data/components/project-status/project-status.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageTitleService } from '../../../../shared/services/page-title.service';
 
 @Component({
   selector: 'app-project-status',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './project-status.component.html',
   styles: ``
 })
-export class ProjectStatusComponent {
-
+export class ProjectStatusComponent implements OnInit{
+  constructor(private readonly pageTitleService: PageTitleService) { }
+  ngOnInit(): void {
+    this.pageTitleService.setTranslatedTitle('PAGETITLE.MASTER_DATA.PROJECT_STATUS');
+  }
 }
