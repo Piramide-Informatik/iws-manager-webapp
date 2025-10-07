ad322331345b3cd85f167d724381ff4587f4ef34
svaleria <svaleria.gonzales@gmail.com>
2025-10-07 19:45:33 -0400

    iws team utils updated

--- src/app/modules/master-data/components/iws-teams/utils/iws-team-utils.ts
+++ src/app/modules/master-data/components/iws-teams/utils/iws-team-utils.ts
@@ -10,6 +10,7 @@ import {
 } from 'rxjs';
 import { TeamIws } from '../../../../../Entities/teamIWS';
 import { TeamIwsService } from '../../../../../Services/team-iws.service';
+import { createNotFoundUpdateError, createUpdateConflictError } from '../../../../shared/utils/occ-error';
 
 @Injectable({ providedIn: 'root' })
 export class TeamIwsUtils {
@@ -31,40 +32,40 @@ export class TeamIwsUtils {
 
     getTeamIwsById(id: number): Observable<TeamIws | undefined> {
         if (!id || id <= 0) {
-        return throwError(() => new Error('Invalid TeamIws ID'));
+            return throwError(() => new Error('Invalid TeamIws ID'));
         }
 
         return this.teamIwsService.getTeamIwsById(id).pipe(
-        catchError((err) => {
-            console.error('Error fetching TeamIws:', err);
-            return throwError(() => new Error('Failed to load TeamIws'));
-        })
+            catchError((err) => {
+                console.error('Error fetching TeamIws:', err);
+                return throwError(() => new Error('Failed to load TeamIws'));
+            })
         );
     }
     refreshTeamIws(): Observable<void> {
         return new Observable<void>((subscriber) => {
-        this.teamIwsService.loadInitialData();
-        subscriber.next();
-        subscriber.complete();
+            this.teamIwsService.loadInitialData();
+            subscriber.next();
+            subscriber.complete();
         });
     }
     //Delete a TeamIws by ID after checking if it's used by any entity
     deleteTeamIws(id: number): Observable<void> {
         return this.checkTeamIwsUsage(id).pipe(
-        switchMap((isUsed) => {
-            if (isUsed) {
-            return throwError(
-                () =>
-                new Error(
-                    'Cannot delete register: it is in use by other entities'
-                )
-            );
-            }
-            return this.teamIwsService.deleteTeamIws(id);
-        }),
-        catchError((error) => {
-            return throwError(() => error);
-        })
+            switchMap((isUsed) => {
+                if (isUsed) {
+                    return throwError(
+                        () =>
+                            new Error(
+                                'Cannot delete register: it is in use by other entities'
+                            )
+                    );
+                }
+                return this.teamIwsService.deleteTeamIws(id);
+            }),
+            catchError((error) => {
+                return throwError(() => error);
+            })
         );
     }
     //Checks if a TeamIws is used by any entity
@@ -74,29 +75,24 @@ export class TeamIwsUtils {
     //Update a TeamIws by ID and updates the internal TeamIws signal
     updateTeamIws(teamIws: TeamIws): Observable<TeamIws> {
         if (!teamIws?.id) {
-        return throwError(() => new Error('Invalid teamIws data'));
+            return throwError(() => new Error('Invalid teamIws data'));
         }
 
         return this.teamIwsService.getTeamIwsById(teamIws.id).pipe(
-        take(1),
-        map((currentTeamIws) => {
-            if (!currentTeamIws) {
-            throw new Error('teamIws not found');
-            }
-            if (currentTeamIws.version !== teamIws.version) {
-            throw new Error(
-                'Version conflict: TeamIws has been updated by another user'
-            );
-            }
-            return teamIws;
-        }),
-        switchMap((validatedTeamIws: TeamIws) =>
-            this.teamIwsService.updateTeamIws(validatedTeamIws)
-        ),
-        catchError((err) => {
-            console.error('Error updating TeamIws:', err);
-            return throwError(() => err);
-        })
+            take(1),
+            switchMap((currentTeamIws) => {
+                if (!currentTeamIws) {
+                    return throwError(() => createNotFoundUpdateError('Title'));
+                }
+                if (currentTeamIws.version !== teamIws.version) {
+                    return throwError(() => createUpdateConflictError('Title'));
+                }
+                return this.teamIwsService.updateTeamIws(teamIws);
+            }),
+            catchError((err) => {
+                console.error('Error updating TeamIws:', err);
+                return throwError(() => err);
+            })
         );
     }
 }
