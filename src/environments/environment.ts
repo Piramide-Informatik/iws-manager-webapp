6b5772d119db7ea92ada0f97c9dbc6894d0e08b5
Omar Selaya <201900837@est.umss.edu>
2025-10-24 15:22:03 -0400

    change enviroment to dev server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     BACK_END_HOST_PROD: 'https://vps53831.alfahosting-vps.de',
-    BACK_END_HOST_DEV: 'https://iws.piramide.de/api/api/v1',
-    BACK_END_HOST_DEV_AUTH: 'https://iws.piramide.de/api/api/auth',
+    BACK_END_HOST_DEV: 'http://89.163.218.44:18080/api/v1',
+    BACK_END_HOST_DEV_AUTH: 'http://89.163.218.44:18080/api/auth',
     BACK_END_LOCALHOST_DEV:'http://localhost:8081/api/v1'
   };
   
