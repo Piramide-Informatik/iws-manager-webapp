a522eb90ec8d3ed24b6b0619f2e16e9c37efe0de
svaleria <svaleria.gonzales@gmail.com>
2025-11-01 22:30:59 -0400

    changed 100.00 by 100

--- src/app/modules/subcontracts/components/subcontracts-detail/project-allocation/project-allocation-modal/project-allocation-modal.component.ts
+++ src/app/modules/subcontracts/components/subcontracts-detail/project-allocation/project-allocation-modal/project-allocation-modal.component.ts
@@ -197,7 +197,7 @@ export class ProjectAllocationModalComponent implements OnInit, OnChanges, OnDes
       subcontractYear: null,
       project: this.allocationForm.value.projectLabel ? this.getProjectSelected(this.allocationForm.value.projectLabel) : null,
       subcontract: this.currentSubcontract!,
-      amount: (this.allocationForm.value.percentage * this.currentSubcontract!.invoiceGross / 100.00),
+      amount: (this.allocationForm.value.percentage * this.currentSubcontract!.invoiceGross / 100),
       share: this.allocationForm.value.percentage
     }
 
@@ -229,7 +229,7 @@ export class ProjectAllocationModalComponent implements OnInit, OnChanges, OnDes
     const raw = this.allocationForm.value;
     const share = Number(raw.percentage);
     const invoiceGross = this.subcontractProject.subcontract?.invoiceGross ?? 0;
-    const amount = share * invoiceGross /100.00;
+    const amount = share * invoiceGross /100;
 
     const updatedProject: SubcontractProject = {
       ...this.subcontractProject,
