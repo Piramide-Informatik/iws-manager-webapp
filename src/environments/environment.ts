28633f87fa84c0f055e9fb2efed254ea81bba9b4
svaleria <svaleria.gonzales@gmail.com>
2025-11-03 11:58:42 -0400

    new environment var

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     BACK_END_HOST_PROD: 'https://vps53831.alfahosting-vps.de',
-    BACK_END_HOST_DEV: 'https://iws.piramide.de/api/api/v1',
+    BACK_END_HOST_DEV: 'http://89.163.218.44/api/v1',
     BACK_END_HOST_DEV_AUTH: 'https://iws.piramide.de/api/api/auth',
     BACK_END_LOCALHOST_DEV:'http://localhost:8081/api/v1'
   };
