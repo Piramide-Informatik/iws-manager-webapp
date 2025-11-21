fb6ac63fbb203c732bc994b9bf6d19d339dbdcec
Omar Selaya <201900837@est.umss.edu>
2025-11-21 09:29:36 -0400

    update

--- src/app/modules/projects/project-routing.module.ts
+++ src/app/modules/projects/project-routing.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ProjectDetailsComponent } from "./components/project-details/project-details.component";
 import { ProjectsOverviewComponent } from "./components/projects-overview/projects-overview.component";
+import { PageProjectComponent } from "./components/page-project/page-project.component";
 
 const routes: Routes = [
     { path: '', component: ProjectsOverviewComponent },
     { path: 'create', component: ProjectDetailsComponent },
+    {
+        path: '',
+        component: PageProjectComponent,
+        children: [
+          {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'project-details/:idProject'
+          },
+          {
+            path: 'project-details/:idProject',
+            component: ProjectDetailsComponent
+          },
+        ]
+    }
 ]
 
 @NgModule({
