a1e74b87beac004936e116a3ebe3f3a712335459
svaleria <svaleria.gonzales@gmail.com>
2025-11-20 22:45:21 -0400

    table implementation

--- src/app/modules/projects/components/project-details/work-packages/work-packages.component.ts
+++ src/app/modules/projects/components/project-details/work-packages/work-packages.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { UserPreferenceService } from './../../../../../Services/user-preferences.service';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProjectPackage } from '../../../../../Entities/ProjectPackage';
 import { UserPreference } from '../../../../../Entities/user-preference';
+import { _, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-work-packages',
@@ -8,16 +11,84 @@ import { UserPreference } from '../../../../../Entities/user-preference';
   templateUrl: './work-packages.component.html',
   styleUrl: './work-packages.component.scss',
 })
-export class WorkPackagesComponent {
-  tableKey: string = 'Allocations';
+export class WorkPackagesComponent implements OnInit {
+  private readonly translate = inject(TranslateService);
+  private readonly userPreferenceService = inject(UserPreferenceService);
+
+  private subscription!: Subscription;
+
+  loading: boolean = true;
+  tableKey: string = 'ProjectPackages';
   projectpackageList!: ProjectPackage[];
   projectpackageColumns: any[] = [];
-  userPreferences: UserPreference = {};
+  projectPackageUserPreferences: UserPreference = {};
   dataKeys = ['packageNo', 'serial', 'packageTitle', 'startDate', 'endDate'];
 
-  onWorkPackagesUserPreferencesChanges(userAllocationsPreferences: any) {
-    localStorage.setItem('userPreferences', JSON.stringify(userAllocationsPreferences));
+  ngOnInit(): void {
+    this.loadColumns();
+    this.projectPackageUserPreferences =
+      this.userPreferenceService.getUserPreferences(
+        this.tableKey,
+        this.projectpackageColumns
+      );
+    this.loading = false;
+    this.subscription = this.translate.onLangChange.subscribe(() => {
+      this.loadColumns();
+      this.projectPackageUserPreferences =
+        this.userPreferenceService.getUserPreferences(
+          this.tableKey,
+          this.projectpackageColumns
+        );
+    });
   }
 
-  handleProjectpackageTableEvents(event: { type: 'create' | 'delete' | 'edit', data?: any }): void {}
+  onWorkPackagesUserPreferencesChanges(userAllocationsPreferences: any) {
+    localStorage.setItem(
+      'userPreferences',
+      JSON.stringify(userAllocationsPreferences)
+    );
+  }
+
+  loadColumns() {
+    this.projectpackageColumns = [
+      {
+        field: 'packageNo',
+        useSameAsEdit: true,
+        header: this.translate.instant(_('PROJECT_PACKAGES.PACKAGE_NUMBER')),
+      },
+      {
+        field: 'serial',
+        type: 'double',
+        customClasses: ['align-right'],
+        header: this.translate.instant(_('PROJECT_PACKAGES.PACKAGE_SERIAL')),
+        filter: { type: 'numeric' },
+      },
+      {
+        field: 'packageTitle',
+        type: 'string',
+        customClasses: ['align-right'],
+        header: this.translate.instant(_('PROJECT_PACKAGES.PACKAGE_NAME')),
+        filter: { type: 'text' },
+      },
+      {
+        field: 'startDate',
+        type: 'date',
+        customClasses: ['align-right'],
+        header: this.translate.instant(_('PROJECT_PACKAGES.START_DATE')),
+        filter: { type: 'date' },
+      },
+      {
+        field: 'endDate',
+        type: 'date',
+        customClasses: ['align-right'],
+        header: this.translate.instant(_('PROJECT_PACKAGES.END_DATE')),
+        filter: { type: 'date' },
+      },
+    ];
+  }
+
+  handleProjectpackageTableEvents(event: {
+    type: 'create' | 'delete' | 'edit';
+    data?: any;
+  }): void {}
 }
