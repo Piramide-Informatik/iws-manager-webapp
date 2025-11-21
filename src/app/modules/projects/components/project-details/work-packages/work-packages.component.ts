1319ecb3d12efb8b211ca0820cb1f492e6c91212
svaleria <svaleria.gonzales@gmail.com>
2025-11-20 21:31:03 -0400

    basic table with no data

--- src/app/modules/projects/components/project-details/work-packages/work-packages.component.ts
+++ src/app/modules/projects/components/project-details/work-packages/work-packages.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { ProjectPackage } from '../../../../../Entities/ProjectPackage';
+import { UserPreference } from '../../../../../Entities/user-preference';
 
 @Component({
   selector: 'app-work-packages',
   standalone: false,
   templateUrl: './work-packages.component.html',
-  styleUrl: './work-packages.component.scss'
+  styleUrl: './work-packages.component.scss',
 })
 export class WorkPackagesComponent {
+  tableKey: string = 'Allocations';
+  projectpackageList!: ProjectPackage[];
+  projectpackageColumns: any[] = [];
+  userPreferences: UserPreference = {};
+  dataKeys = ['packageNo', 'serial', 'packageTitle', 'startDate', 'endDate'];
 
+  onWorkPackagesUserPreferencesChanges(userAllocationsPreferences: any) {
+    localStorage.setItem('userPreferences', JSON.stringify(userAllocationsPreferences));
+  }
+
+  handleProjectpackageTableEvents(event: { type: 'create' | 'delete' | 'edit', data?: any }): void {}
 }
