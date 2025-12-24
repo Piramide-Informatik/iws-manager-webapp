c6903ea499a72b63846d53f1fa3b4af921224f79
svaleria <svaleria.gonzales@gmail.com>
2025-12-24 15:27:59 -0400

    employee select

--- src/app/modules/projects/components/employee-project/components/employee-detail-modal/employee-detail-modal.component.ts
+++ src/app/modules/projects/components/employee-project/components/employee-detail-modal/employee-detail-modal.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { EmployeeUtils } from '../../../../../customer/sub-modules/employee/utils/employee.utils';
+import { Employee } from '../../../../../../Entities/employee';
 
 @Component({
   selector: 'app-employee-detail-modal',
@@ -9,11 +12,17 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './employee-detail-modal.component.scss'
 })
 export class EmployeeDetailModalComponent implements OnInit, OnChanges {
+  private readonly employeeUtils = inject(EmployeeUtils);
+  private subscriptions = new Subscription();
+
   @Input() modalType: 'create' | 'edit' | 'delete' = 'create';
   @Input() visibleModal: boolean = false;
   @Output() isVisibleModal = new EventEmitter<boolean>();
+
   isLoading = false;
   projectId = '';
+  employees: Employee[] = [];
+
   constructor(private readonly activatedRoute: ActivatedRoute) { }
 
   readonly createEmployeeDetailsForm = new FormGroup({
@@ -27,10 +36,48 @@ export class EmployeeDetailModalComponent implements OnInit, OnChanges {
     this.activatedRoute.params.subscribe(params => {
       this.projectId = params['idProject'];
     });
+    this.loadEmployees();
+
+    console.log(this.employees);
   }
 
   ngOnChanges(): void {
 
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private loadEmployees(): void {
+    const sub = this.employeeUtils.getEmployeesSortedByName().subscribe({
+      next: (employees: Employee[]) => {
+        this.employees = employees.map(employee => ({
+          ...employee,
+          displayName: this.getEmployeeDisplayName(employee)
+        }));
+      },
+      error: (error) => {
+        console.error('Error loading employees:', error);
+        this.employees = [];
+      }
+    });
+    this.subscriptions.add(sub);
+  }
+
+  private getEmployeeDisplayName(employee: Employee): string {
+    if (employee.firstname && employee.lastname) {
+      return `${employee.firstname} ${employee.lastname}`;
+    } else if (employee.firstname) {
+      return employee.firstname;
+    } else if (employee.lastname) {
+      return employee.lastname;
+    } else if (employee.label) {
+      return employee.label;
+    } else if (employee.employeeno) {
+      return `Employee #${employee.employeeno}`;
+    }
+    return `Employee ID: ${employee.id}`;
   }
 
   onSubmit(): void {
@@ -58,5 +105,20 @@ export class EmployeeDetailModalComponent implements OnInit, OnChanges {
 
   removeFromProject(): void {
     this.closeAndReset();
+  }
+
+  // Método para cuando cambia la selección del empleado
+  onEmployeeChange(event: any): void {
+    const employeeId = event.value;
+    const selectedEmployee = this.employees.find(emp => emp.id === employeeId);
+
+    if (selectedEmployee) {
+      this.createEmployeeDetailsForm.patchValue({
+        employeeNo: selectedEmployee.employeeno || null,
+        // Si quieres llenar automáticamente otros campos:
+        // hourlyrate: selectedEmployee.hourlyRate || '',
+        // qualificationkmui: selectedEmployee.qualificationkmui || ''
+      });
+    }
   }
 }
